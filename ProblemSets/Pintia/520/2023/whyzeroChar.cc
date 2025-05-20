eb423527d9e889c1fd6b427ab73af7992d16a66d
RabbitXWindows <xiali123.ch@gmail.com>
2025-05-20 19:25:02 +0800

    2025 520.

--- ProblemSets/Pintia/520/2023/whyzeroChar.cc
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-int main() {
-    string str1( "0" );
-    char str2[] = "0";
-    char str3[10]{ '\0' };
-    // str3 = str1.c_str(); // str3 is not modifiable
-    return 0;
-}
